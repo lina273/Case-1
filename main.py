5fd5d3b0386f2459c9828043f9c6e9f1445cce3e
lina273 <tl9072@mci4me.at>
2024-12-18 16:16:23 +0100

    Erstellung der Klassen

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-print ("Hello world")
-print("2 Hello world")
-print("3 Hello world")
+class User:
+
+    def __init__ (self,user_id,user_name):
+        self.user_id=user_id
+        self.user_name=user_name
+
+    def __str__(self):
+        return f"{self.name} ({self.user_id})"
+    
+    ##def
+
+
+class Device:
+    def __init__(self, device_id: int, device_name: str, responsible_person: User, maintenance_interval: int, maintenance_cost: float):
+        self.device_id = device_id
+        self.device_name = device_name
+        self.responsible_person = responsible_person
+        ## self.__last_update = 
+        ## self.__creation_date = 
+        ##self.end_of_life = 
+        ##self.first_maintenance = 
+        ##self.next_maintenance = 
+        ##self.__maintenance_interval = 
+        ##self.__maintenance_cost = 
+
+
+    def __str__(self):
+        return f"Device {self.device_name} (Responsible: {self.responsible_person.name})"
+
+
+        
